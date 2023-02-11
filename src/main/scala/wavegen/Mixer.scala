876395e280fd674d31a808b1e1acd29a08273779
Kai Tamkun <ktamkun@ucsc.edu>
2023-02-11 14:48:36 -0800

    Mixer appears to work

--- src/main/scala/wavegen/Mixer.scala
+++ src/main/scala/wavegen/Mixer.scala
@@ -11,8 +11,8 @@ class MixerDebug(channelCount: Int, width: Int) extends Bundle {
 	val max = Output(FixedPoint((width + extra).W, extra.BP))
 }
 
-class Mixer(channelCount: Int, width: Int, memorySize: Int) extends Module {
-	val sInit :: sSumming :: Nil = Enum(2)
+class Mixer(channelCount: Int, width: Int, memorySize: Int, batchSize: Int = 1) extends Module {
+	val sSumming :: sAdjusting :: sDone :: Nil = Enum(3)
 
 	val io = IO(new Bundle {
 		val in  = Vec(channelCount, Flipped(Decoupled(FixedPoint(width.W, 0.BP))))
@@ -22,16 +22,16 @@ class Mixer(channelCount: Int, width: Int, memorySize: Int) extends Module {
 
 	val bigger = width + log2Ceil(channelCount)
 	
-	val state = RegInit(sInit)
+	val state = RegInit(sSumming)
 	io.debug.state := state
 	
 	io.out.valid := false.B
-	io.out.bits := .0.F(0.BP)
+	io.out.bits := 0.F(0.BP)
 	
 	io.in.foreach { _.ready := true.B }
 	
 	val summer = Module(new FPSummer(channelCount, width))
-	val maxReg = RegInit(summer.makeOut(0.0))
+	val maxReg = RegInit(summer.makeOut(0))
 
 	io.debug.max := maxReg
 
@@ -41,23 +41,49 @@ class Mixer(channelCount: Int, width: Int, memorySize: Int) extends Module {
 
 	val allValid = io.in.foldLeft(true.B)(_ && _.valid)
 
-	val memory = Reg(Vec(memorySize, summer.outType))
-	val index = RegInit(0.U(log2Ceil(memorySize).W))
+	val memory = Mem(memorySize, summer.outType)
 
-	when (state === sInit) {
+	val enableIndex = Reg(Bool())
+	val resetIndex = Reg(Bool())
+	val (index, indexWrap) = Counter(0 until memorySize, enableIndex, resetIndex)
+
+	val maxPossible = Fill(width, 1.U(1.W))
+
+	enableIndex := false.B
+	resetIndex := false.B
+
+	when (state === sSumming) {
 		when (allValid) {
 			memory(index) := summer.io.out
 
+			// FixedPoints appear to be always signed.
 			when (maxReg.asUInt < summer.io.out.asUInt) {
 				maxReg := summer.io.out
 			}
 
-			when (index === (memorySize - 1).U) {
-				index := 0.U
-				state := sSumming
+			when (indexWrap) {
+				resetIndex := true.B
+				when (maxPossible < maxReg.asUInt) {
+					state := sAdjusting
+				} .otherwise {
+					state := sDone
+				}
 			} .otherwise {
-				index := index + 1.U
+				enableIndex := true.B
 			}
+		}
+	} .elsewhen (state === sAdjusting) {
+		val newValue = (memory(index).asUInt * maxPossible / maxReg.asUInt).asFixedPoint(0.BP)
+		memory(index) := newValue
+		io.out.valid := true.B
+		io.out.bits := newValue
+
+		when (indexWrap) {
+			resetIndex := true.B
+			state := sDone
+			printf(p"Done\n")
+		} .otherwise {
+			enableIndex := true.B
 		}
 	}
 }
