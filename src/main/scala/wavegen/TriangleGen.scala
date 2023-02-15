8fda4e68e8af5cd461ebb4deac5f5cf27c462477
Kai Tamkun <ktamkun@ucsc.edu>
2023-02-15 13:32:13 -0800

    Finish (?) TriangleGen

--- src/main/scala/wavegen/TriangleGen.scala
+++ src/main/scala/wavegen/TriangleGen.scala
@@ -3,18 +3,57 @@ package wavegen
 import chisel3._
 import chisel3.util._
 
-class TriangleGen(period: Int, resolution: Int, startHigh: Boolean = false) extends Module {
-	require(1 < period)
+class TriangleGen(resolution: Int, startHigh: Boolean = false)(implicit clockFreq: Int) extends Module {
+	require(2 <= clockFreq)
 	require(1 < resolution)
-	val outWidth = log2Ceil(resolution + 1).W
+	val outWidth = log2Ceil(resolution + 1)
+	val clockWidth = log2Ceil(clockFreq + 1)
 
 	val io = IO(new Bundle {
 		val pause = Input(Bool())
-		val out = Output(UInt(outWidth))
+		val freq  = Input(UInt(clockWidth.W))
+		val out   = Output(UInt(outWidth.W))
 	})
-
 
 	val rising = RegInit((!startHigh).B)
 
+	val clocker = Module(new Clocker)
+	clocker.io.enable := !io.pause
 
+	val freq = io.freq * ((resolution << 1) + 1).U
+	val steps = Wire(UInt(clockWidth.W))
+
+	when (freq <= clockFreq.U) {
+		clocker.io.freq := freq
+		steps := 1.U
+	} .otherwise {
+		clocker.io.freq := clockFreq.U
+		steps := freq / clockFreq.U
+	}
+
+	val out = RegInit(0.U(outWidth.W))
+
+	when (clocker.io.tick) {
+		when (rising) {
+			when (out === resolution.U) {
+				rising := false.B
+				out := out - steps
+			} .elsewhen(resolution.U < out +& steps) {
+				out := resolution.U
+			} .otherwise {
+				out := out + steps
+			}
+		} .otherwise {
+			when (out === 0.U) {
+				rising := true.B
+				out := out + steps
+			} .elsewhen (out < steps) {
+				out := 0.U
+			} .otherwise {
+				out := out - steps
+			}
+		}
+	}
+
+	io.out := out
 }
