e62479be97414568ca847baf9d281fec771b80d9
Kai Tamkun <ktamkun@ucsc.edu>
2023-03-07 03:58:59 -0800

    Improve WNS

--- src/main/scala/wavegen/misc/ImageOutput.scala
+++ src/main/scala/wavegen/misc/ImageOutput.scala
@@ -4,7 +4,7 @@ import chisel3._
 import chisel3.util._
 import scala.math.{sin, floor, Pi}
 
-class ImageOutput(implicit clockFreq: Int) extends Module {
+class ImageOutput extends Module {
 	val imageWidth  = 160
 	val imageHeight = 144
 
@@ -105,9 +105,10 @@ class ImageOutput(implicit clockFreq: Int) extends Module {
 
 			val y = (io.y - top.U - sines((counter + charIndex)(6, 0))) >> shift.U
 
-			font.io.char := char
-			font.io.x    := x(2, 0)
-			font.io.y    := y(2, 0)
+			// The RegNexts here are a WNS hack that appears to have no impact on the visuals.
+			font.io.char := RegNext(char)
+			font.io.x    := RegNext(x(2, 0))
+			font.io.y    := RegNext(y(2, 0))
 
 			when (0.U <= x && x < width.U && 0.U <= y && y < height.U && font.io.out) {
 				io.green := 255.U - colors.io.green
